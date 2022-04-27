a9374a315ec7dabfa09f7153c2566ff37ca7668a
Muntaha Islam <muntaha.islam0019@gmail.com>
2022-04-27 18:21:49 +0600

    Solved assignment 3

---
+++ Assignments/control-assignment-problem/assignment.js
@@ -0,0 +1,36 @@
+const randomNumber = Math.random(); // produces random number between 0 (including) and 1 (excluding)
+
+if (randomNumber > 0.7) {
+  alert('The first number is bigger than 0.7!');
+} else {
+  alert('The first number is smaller than 0.7!');
+}
+
+numbers = [19, 47, 10, 7, 13];
+
+alert('Starting for loop');
+for (const number of numbers) {
+  alert(number);
+}
+
+alert('Starting while loop');
+let i = numbers.length - 1;
+while (i >= 0) {
+  alert(numbers[i--]);
+}
+
+const anotherRandomNumber = Math.random();
+
+if (
+  (randomNumber > 0.7 && anotherRandomNumber > 0.7) ||
+  randomNumber < 0.2 ||
+  anotherRandomNumber < 0.2
+) {
+  alert(
+    `The numbers are ${randomNumber} and ${anotherRandomNumber}. Thus, they satisfy the conditions.`
+  );
+} else {
+  alert(
+    `The numbers are ${randomNumber} and ${anotherRandomNumber}. Thus, they do not satisfy the conditions.`
+  );
+}
