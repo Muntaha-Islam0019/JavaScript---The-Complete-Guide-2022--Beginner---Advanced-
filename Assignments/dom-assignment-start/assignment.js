ecde5434c79014fd1fb024bd5b6521d5e41e4c9b
Muntaha Islam <muntaha.islam0019@gmail.com>
2022-05-05 11:56:13 +0600

    Solved assignment 5

---
+++ Assignments/dom-assignment-start/assignment.js
@@ -0,0 +1,17 @@
+const taskOneFirst = document.getElementById('task-1');
+taskOneFirst.style.color = 'white';
+taskOneFirst.style.backgroundColor = 'black';
+
+const taskOneSecond = document.querySelector('li');
+taskOneSecond.style.color = 'white';
+taskOneSecond.style.backgroundColor = 'black';
+
+const taskTwoFirst = document.querySelector('title');
+taskTwoFirst.textContent = 'Assignment - Solved!';
+
+const taskTwoSecond = document.head;
+const taskTwoSecondQuery = taskTwoSecond.querySelector('title');
+taskTwoSecondQuery.textContent = 'Assignment - Solved!';
+
+const taskThree = document.querySelector('h1');
+taskThree.textContent = 'Assignment - Solved!';
